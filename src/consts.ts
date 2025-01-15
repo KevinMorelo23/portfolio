7c0749cfbb986d010cb41e1ebc7693adf7b01158
kevinmorelo13 <kmorelo89@gmail.com>
2025-01-15 14:09:08 -0500

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Kevin Morelo Ingeniero en Sistemas y data science";
+export const SITE_DESCRIPTION = "Bienvedio a mi portafolio";
